d7dd7659dfb26032371cb085148e9b0ebe45ad2a
Adam Brown <downtownadambrown@gmail.com>
2024-01-06 14:44:40 -0500

    adds mouseover to header, adds notification banner component, adds fonts

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ const SectionContainer = ({
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-16">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24">
       <SectionContainer>
         <MainBanner />
         <Section alt="" src="" />
